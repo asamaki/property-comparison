22626ec995c822688d1ab5eef0778078ab5e08ed
Ikai Masahiro <>
2020-07-23 18:52:33 +0900

    puppeteerを追加

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,21 @@ const express = require('express')
 
 const app = express()
 
-app.get('/users', (req, res) => {
-  // 本来はここでFireStoreとかからデータ取ってくるよ
-  const users = [
-    { id: 1, name: 'イリヤ' },
-    { id: 2, name: '美遊' },
-    { id: 3, name: 'クロエ' },
-    { id: 4, name: 'オルタ' },
-    { id: 5, name: 'マシュ' },
-  ]
+app.get('/users', async (req, res) => {
+  const puppeteer = require('puppeteer')
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  })
+  const page = await browser.newPage()
+
+  await page.goto('https://www.google.com/', { waitUntil: 'networkidle0' })
+
+  const content = await page.content()
+  browser.close()
 
   // データを返却
-  res.send(JSON.stringify(users))
+  res.send(JSON.stringify(content))
 })
 
 const api = functions.region('asia-northeast1').https.onRequest(app)
